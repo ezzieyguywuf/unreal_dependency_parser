360d31e8eb26653403591dc8b2cf1380bc8e981a
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-06-11 16:28:13 -0400

    Added basic xml parsing and debugging
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- parser.py
+++ parser.py
@@ -1,9 +1,26 @@
 import argparse
+import xml.etree.ElementTree as ET
 
-if __name__=="__main__":
+# Expects a filename, which will contain the xml data. No error-handling is
+# currently incorporated - so make sure it's an actual xml file
+def parseFile(fname: str) -> ET:
+    print("trying to parse {}".format(fname))
+    return ET.parse(fname)
+
+def parseArguments():
     parser = argparse.ArgumentParser(description='Parse Unreal Engine deps')
     parser.add_argument('dependency_file', metavar='FILE', type=str,
                         help='The xml file containing the Unreal Engine dependencies')
 
-    args = parser.parse_args()
+    return parser.parse_args()
+
+
+if __name__=="__main__":
+    args = parseArguments()
+
     print(args.dependency_file)
+    tree = parseFile(args.dependency_file)
+    root = tree.getroot()
+
+    print(root.tag)
+    print(root.attrib)
