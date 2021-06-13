66706404ca18faa358c43f9f9fb1eb8e47129310
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-06-13 14:06:30 -0400

    Added processor.py module and moved logic out of main
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import argparse
-import gzip
-import hashlib
 import parser
+import processor
 import requests
-import os
-import os.path
-import shutil
 
 def parseArguments():
     parser = argparse.ArgumentParser(description='Parse Unreal Engine deps')
@@ -13,7 +9,6 @@ def parseArguments():
                         help='The xml file containing the Unreal Engine dependencies')
 
     return parser.parse_args()
-
 
 if __name__=="__main__":
     args = parseArguments()
@@ -29,23 +24,4 @@ if __name__=="__main__":
         print("Downloading: " + url)
         req = requests.get(url)
 
-        data = gzip.decompress(req.content)
-        check_hash = hashlib.sha1(data)
-        if pack.hash == check_hash.hexdigest():
-            for blob in pack.blobs:
-                blobData = data[blob.offset:blob.offset + blob.size]
-                blobHash = hashlib.sha1(blobData)
-
-                if blob.hash == blobHash.hexdigest():
-                    for path in blob.paths:
-                        dirname = os.path.dirname(path.filePath)
-                        # This will create the directory if it doesn't exist
-                        os.makedirs(dirname, exist_ok=True)
-                        with open(path.filePath, 'wb') as outfile:
-                            outfile.write(blobData)
-                        print("    saved to: " + path.filePath)
-                else:
-                    print("    BLOB HASH DOES NOT MATCH. DOING NOTHING")
-                    print("    (blob hash = {})".format(blobHash))
-        else:
-            print("PACK HASH DOES NOT MATCH. DOING NOTHING.")
+        processor.processPack(pack, req)
---
+++ processor.py
@@ -0,0 +1,33 @@
+import gzip
+import hashlib
+import os
+import os.path
+import shutil
+
+def processPaths(blob, blobData):
+    for path in blob.paths:
+        dirname = os.path.dirname(path.filePath)
+        # This will create the directory if it doesn't exist
+        os.makedirs(dirname, exist_ok=True)
+        with open(path.filePath, 'wb') as outfile:
+            outfile.write(blobData)
+        print("    saved to: " + path.filePath)
+
+def processBlobs(pack, data):
+    for blob in pack.blobs:
+        blobData = data[blob.offset:blob.offset + blob.size]
+        blobHash = hashlib.sha1(blobData)
+
+        if blob.hash == blobHash.hexdigest():
+            processPaths(blob, blobData)
+        else:
+            print("    BLOB HASH DOES NOT MATCH. DOING NOTHING")
+            print("    (blob hash = {})".format(blobHash))
+
+def processPack(pack, req):
+        data = gzip.decompress(req.content)
+        check_hash = hashlib.sha1(data)
+        if pack.hash == check_hash.hexdigest():
+            processBlobs(pack, data)
+        else:
+            print("PACK HASH DOES NOT MATCH. DOING NOTHING.")
